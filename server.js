0b63a8b07faf97624d84552948d5ed30f268c233
GowthamTechjays <gowtham.e@techjays.com>
2021-11-30 19:39:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 8000
+
+app.use(express.static(path.join(__dirname, "hub", "build")))
+
+app.listen(PORT, () => {
+    console.log('App listen on Port' + PORT)
+})
